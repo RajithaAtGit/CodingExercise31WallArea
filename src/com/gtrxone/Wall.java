2085179298ba5fa0ef5d388ea1cd52b997f2fa82
Goonawardana R N W <gtrxone@gmail.com>
2021-11-21 12:43:20 +0530

    Initial commit

---
+++ src/com/gtrxone/Wall.java
@@ -0,0 +1,41 @@
+package com.gtrxone;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(0, 0);
+    }
+
+    public Wall(double width, double height) {
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    public double getArea(){
+        return width*height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else
+            this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else
+            this.height = height;
+    }
+}
